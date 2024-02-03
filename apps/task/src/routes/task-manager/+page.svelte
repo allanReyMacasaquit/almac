b92d5ec11c93e1508150d41be1edd68c9755ef91
allan <allanrey.macasaquit@gmail.com>
2024-02-02 22:24:17 -0500

    using reactive statement

--- apps/task/src/routes/task-manager/+page.svelte
+++ apps/task/src/routes/task-manager/+page.svelte
@@ -1,1 +1,17 @@
-<h1 class="text-white">I am task manager</h1>
+<script>
+	let count = 0;
+	$: {
+		if (count > 10) {
+			console.log('I am the winner');
+		}
+	}
+</script>
+
+<h1 class="text-white">Click me {count}</h1>
+<button
+	on:click={() => {
+		count++;
+	}}
+>
+	Increment
+</button>
