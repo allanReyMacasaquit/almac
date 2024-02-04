df1759e1b35542c2c459d1419fb0b18b054f89e9
allan <allanrey.macasaquit@gmail.com>
2024-02-03 22:54:09 -0500

    start Function and stop function

--- apps/task/src/stores/tasks.js
+++ apps/task/src/stores/tasks.js
@@ -1,12 +1,16 @@
 import { writable } from "svelte/store";
 
-let i = 0;
 const createStore = () => {
-  const taskList = writable([]);
+  const taskList = writable([], () => {
+    let i = 0;
+    const id = setInterval(() => {
+      taskList.update((list) => [...list, i++]);
+    }, 2000);
 
-  setInterval(() => {
-    taskList.update((list) => [...list, i++]);
-  }, 2000);
+    return () => {
+      clearInterval(id);
+    };
+  });
 
   return taskList;
 };
