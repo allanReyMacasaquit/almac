3823b7bbebaaf2e1567c0f04b35342ca3b86934d
allan <allanrey.macasaquit@gmail.com>
2024-02-09 21:52:56 -0500

    set up initial routes

--- apps/messaging/src/components/sidebar/navLinks.js
+++ apps/messaging/src/components/sidebar/navLinks.js
@@ -3,6 +3,7 @@ import TiGroupOutline from "svelte-icons/ti/TiGroupOutline.svelte";
 import TiCogOutline from "svelte-icons/ti/TiCogOutline.svelte";
 import TiBell from "svelte-icons/ti/TiBell.svelte";
 import TiZoomOutline from "svelte-icons/ti/TiZoomOutline.svelte";
+import FaShareAlt from "svelte-icons/fa/FaShareAlt.svelte";
 
 const navLinks = [
   {
@@ -12,23 +13,28 @@ const navLinks = [
   },
   {
     name: "Profile",
-    href: "/",
+    href: "/profile",
     icon: TiGroupOutline
   },
   {
     name: "More",
-    href: "/",
+    href: "/more",
     icon: TiCogOutline
   },
   {
     name: "Notifications",
-    href: "/",
+    href: "/notifications",
     icon: TiBell
   },
   {
     name: "Discover",
-    href: "/task-manager",
+    href: "/discoverer",
     icon: TiZoomOutline
+  },
+  {
+    name: "Share your thoughts",
+    href: "/thoughts",
+    icon: FaShareAlt
   }
 ];
 
