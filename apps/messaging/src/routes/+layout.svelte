4bbac693e96c14a15ffe98056f1b557d87068c95
allan <allanrey.macasaquit@gmail.com>
2024-02-19 15:14:02 -0500

    add UIContext

--- apps/messaging/src/routes/+layout.svelte
+++ apps/messaging/src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
   import AuthContext from "$components/context/auth/AuthContext.svelte";
+  import UIContext from "$components/context/ui/UIContext.svelte";
   import SnackbarContainer from "$components/snackbar/SnackbarContainer.svelte";
   import "../app.css";
 </script>
 
-<AuthContext>
-  <SnackbarContainer />
-  <slot />
-</AuthContext>
+<UIContext>
+  <AuthContext>
+    <SnackbarContainer />
+    <slot />
+  </AuthContext>
+</UIContext>
